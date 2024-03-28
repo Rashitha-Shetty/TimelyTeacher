23d2a6d2c58740e51a6832196e0d1bfcde71fe66
Rashitha-Shetty <shettyrashitha22@gmail.com>
2024-03-28 07:48:38 +0530

    hehe

--- App_TT/views.py
+++ App_TT/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-def home(request):
+def login(request):
     return render(request, 'login.html')
+    
+
+
+
+
+
+
+def home(request):
+    pass
+
